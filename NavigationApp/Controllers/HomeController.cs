cb3b5037078624d2f2e970a466f0310d748e10ba
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-12-04 01:42:20 +0300

    lessons 27-28

--- NavigationApp/Controllers/HomeController.cs
+++ NavigationApp/Controllers/HomeController.cs
@@ -1,17 +1,81 @@
-﻿using System;
+﻿using NavigationApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace NavigationApp.Controllers
 {
     public class HomeController : Controller
     {
+        SoccerContext db = new SoccerContext();
         public ActionResult Index()
         {
+            var players = db.Players.Include(p => p.Team); 
+            return View(players.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.Teams = teams;
             return View();
         }
+
+
+
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            db.Players.Add(player);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            //Находим в бд футболиста
+            Player player = db.Players.Find(id);
+            if (player != null)
+            {
+            SelectList teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+            ViewBag.Teams = teams;
+            return View(player);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Player player = db.Players.Find(id);
+            if (player != null)
+            {
+                db.Players.Remove(player);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
 
         public ActionResult About()
         {
